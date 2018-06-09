421868d5901ffee4c9a81198be07bdf6fc920f6c
antho <krobross_57@hotmail.fr>
2018-06-09 22:11:43 +0200

    création et ajout du fichier insert.php

---
+++ App/Backend/Modules/News/Views/insert.php
@@ -0,0 +1,38 @@
+<div class="container">
+  <div class="row">
+    <div id="chapteradding" class="col-lg-12 mainContent">
+        <h2 class="mainTitles">Ajouter un chapitre</h2>
+        <div class="container">
+          <div class="row">
+        <form class="col-lg-12" action="" method="post">
+
+
+            <?= $form ?>
+
+
+
+          <input type="submit" value="Ajouter" />
+
+        </form>
+      </div>
+    </div>
+    </div>
+  </div>
+</div>
+
+<script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=41iw6hf6vpe8spj6jwl6c9zun3tdu5vuvx4xdusyruuahpnu"></script>
+
+<script> tinymce.init({
+  selector: '#contenu',
+  height: 400,
+  width: '90%',
+  menubar: false,
+  branding: false,
+  elementpath: false,
+  plugins: [
+    'advlist autolink lists link image charmap print preview anchor textcolor',
+    'searchreplace visualblocks code fullscreen',
+    'insertdatetime media table contextmenu paste code help wordcount'
+  ],
+  toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+});</script>
