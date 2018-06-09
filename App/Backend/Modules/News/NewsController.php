efcc03c8133889c779570e3971dfacab828e8176
antho <krobross_57@hotmail.fr>
2018-06-09 17:12:00 +0200

    création et ajout du fichier NewsController.php

---
+++ App/Backend/Modules/News/NewsController.php
@@ -0,0 +1,259 @@
+<?php
+namespace App\Backend\Modules\News;
+
+use \OCFram\BackController;
+use \OCFram\HTTPRequest;
+use \Entity\News;
+use \Entity\Comment;
+use \FormBuilder\CommentFormBuilder;
+use \FormBuilder\NewsFormBuilder;
+use \OCFram\FormHandler;
+
+class NewsController extends BackController
+{
+  public function executeDelete(HTTPRequest $request)
+  {
+
+    $newsId = $request->getData('id');
+
+    $this->managers->getManagerOf('News')->delete($newsId);
+    $this->managers->getManagerOf('Comments')->deleteFromNews($newsId);
+
+    $this->app->user()->setFlash('Le chapitre a bien été supprimé !');
+
+    $this->app->httpResponse()->redirect('/admin/');
+
+  }
+
+  public function executeDeleteComment(HTTPRequest $request)
+  {
+    $this->managers->getManagerOf('Comments')->delete($request->getData('id'));
+
+    $this->app->user()->setFlash('Le commentaire a bien été supprimé !');
+
+    $this->app->httpResponse()->redirect('/admin/');
+  }
+
+  public function executeIndex(HTTPRequest $request)
+  {
+      $newsManager = $this->managers->getManagerOf('News');
+      $commentsManager = $this->managers->getManagerOf('Comments');
+      $nombreCaracteres = $this->app->config()->get('nombre_caracteres');
+
+      $newsPubliees = $newsManager->count();
+      $commentsAModerer= count($commentsManager->countMarkedComments(true));
+
+
+      $lastNews = $newsManager->getList(0, 1);
+      $lastComments = $commentsManager->getList(0, 1);
+
+
+
+      foreach ($lastNews as $news) {
+
+        if (strlen($news->contenu()) > $nombreCaracteres)
+        {
+          $debut = substr($news->contenu(), 0, $nombreCaracteres);
+          $debut = substr($debut, 0, strrpos($debut, ' ')) . ' ...';
+
+          $news->setContenu($debut);
+        }
+
+      }
+
+      foreach ($lastComments as $comments) {
+
+        if (strlen($comments->contenu()) > $nombreCaracteres)
+        {
+          $debut = substr($news->contenu(), 0, $nombreCaracteres);
+          $debut = substr($debut, 0, strrpos($debut, ' ')) . ' ...';
+
+          $comments->setContenu($debut);
+        }
+
+      }
+
+
+      $this->page->addVar('title', 'Tableau de bord');
+      $this->page->addVar('newsPubliees', $newsPubliees);
+      $this->page->addVar('commentsAModerer', $commentsAModerer);
+      $this->page->addVar('lastNews', $lastNews);
+      $this->page->addVar('lastComments', $lastComments);
+
+  }
+
+  public function executeIndexNews(HTTPRequest $request)
+  {
+    $newsManager = $this->managers->getManagerOf('News');
+
+
+    $nombreNews = $this->app->config()->get('nombre_news');
+
+
+    $nombreTotalNews = $newsManager->count();
+
+    $newsParPage = $nombreNews;
+
+    $page = intval($request->getData('page'));
+    $nombrePagesNews = ceil($nombreTotalNews/$newsParPage);
+
+
+    if ($page > 0 && $page <= $nombrePagesNews) {
+      $pageCourante = $page;
+    }
+
+    else {
+      $pageCourante = 1;
+    }
+
+    $listeNews = $newsManager->getList((($pageCourante-1)*$newsParPage), $newsParPage);
+
+
+
+    $this->page->addVar('title', 'Liste des chapitres');
+    $this->page->addVar('listeNews', $listeNews);
+    $this->page->addVar('nombreNews', $nombreNews);
+    $this->page->addVar('nombrePagesNews', $nombrePagesNews);
+    $this->page->addVar('pageCourante', $pageCourante);
+
+  }
+
+  public function executeIndexComments(HTTPRequest $request)
+  {
+
+    $commentsManager = $this->managers->getManagerOf('Comments');
+
+
+    $nombreComments = $this->app->config()->get('nombre_comments');
+
+
+    $nombreTotalComments = $commentsManager->count();
+
+    $commentsParPage = $nombreComments;
+    $page = intval($request->getData('page'));
+
+    $nombrePagesComments = ceil($nombreTotalComments/$commentsParPage);
+
+    if ($page > 0 && $page <= $nombrePages) {
+      $pageCourante = $page;
+    }
+
+    else {
+      $pageCourante = 1;
+    }
+
+    $listeComments = $commentsManager->getMarkedComment(true, 0, $nombreComments);
+
+    $this->page->addVar('title', 'Commentaires en attente de modération');
+    $this->page->addVar('comments', $listeComments );
+    $this->page->addVar('nombreComments', $nombreComments);
+    $this->page->addVar('nombrePagesComments', $nombrePagesComments);
+    $this->page->addVar('pageCourante', $pageCourante);
+
+  }
+
+  public function executeInsert(HTTPRequest $request)
+  {
+    $this->processForm($request);
+
+    $this->page->addVar('title', 'Ajouter un chapitre');
+  }
+
+  public function executeUpdate(HTTPRequest $request)
+  {
+    $this->processForm($request);
+
+    $this->page->addVar('title', 'Modifier un chapitre');
+  }
+
+  public function executeUpdateComment(HTTPRequest $request)
+  {
+    $this->page->addVar('title', 'Modifier un commentaire');
+
+    if ($request->method() == 'POST')
+    {
+      $comment = new Comment([
+        'id' => $request->getData('id'),
+        'auteur' => $request->postData('auteur'),
+        'contenu' => $request->postData('contenu')
+      ]);
+    }
+    else
+    {
+      $comment = $this->managers->getManagerOf('Comments')->get($request->getData('id'));
+    }
+
+    $formBuilder = new CommentFormBuilder($comment);
+    $formBuilder->build();
+
+    $form = $formBuilder->form();
+
+    $formHandler = new FormHandler($form, $this->managers->getManagerOf('Comments'), $request);
+
+    if ($formHandler->process())
+    {
+      $this->app->user()->setFlash('Le commentaire a bien été modifié');
+
+      $this->app->httpResponse()->redirect('/admin/');
+    }
+
+    $this->page->addVar('form', $form->createView());
+  }
+
+  public function processForm(HTTPRequest $request)
+  {
+    if ($request->method() == 'POST')
+    {
+      $news = new News([
+        'auteur' => $request->postData('auteur'),
+        'titre' => $request->postData('titre'),
+        'contenu' => $request->postData('contenu')
+      ]);
+
+      if ($request->getExists('id'))
+      {
+        $news->setId($request->getData('id'));
+      }
+    }
+    else
+    {
+      // L'identifiant de la news est transmis si on veut la modifier
+      if ($request->getExists('id'))
+      {
+        $news = $this->managers->getManagerOf('News')->getUnique($request->getData('id'));
+      }
+      else
+      {
+        $news = new News;
+      }
+    }
+
+    $formBuilder = new NewsFormBuilder($news);
+    $formBuilder->build();
+
+    $form = $formBuilder->form();
+
+    $formHandler = new FormHandler($form, $this->managers->getManagerOf('News'), $request);
+
+    if ($formHandler->process())
+    {
+      $this->app->user()->setFlash($news->isNew() ? 'Le chapitre a bien été ajouté !' : 'Le chapitre a bien été modifié !');
+
+      $this->app->httpResponse()->redirect('/admin/');
+    }
+
+    $this->page->addVar('form', $form->createView());
+  }
+
+  public function executeUnmarkComment(HTTPRequest $request)
+  {
+
+    $comment = $this->managers->getManagerOf('Comments')->get($request->getData('id'));
+
+    $comment->setMark(false);
+
+    $this->managers->getManagerOf('Comments')->mark($comment);
+
+    $this->app->httpResponse()->redirect('/admin/comments.html');
+  }
+}
