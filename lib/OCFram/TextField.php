261bca0935f24816c0572ba052d10519055387cd
antho <krobross_57@hotmail.fr>
2018-06-09 23:42:49 +0200

    création et ajout du fichier TextField.php git status

---
+++ lib/OCFram/TextField.php
@@ -0,0 +1,27 @@
+<?php
+namespace OCFram;
+
+class TextField extends Field
+{
+
+  public function buildWidget()
+  {
+    $widget = '';
+
+    if (!empty($this->errorMessage))
+    {
+      $widget .= $this->errorMessage.'<br />';
+    }
+
+    $widget .= '<label for="'.$this->for.'">'.$this->label.'</label><textarea placeholder= "'.$this->placeHolder.'"name="'.$this->name.'" id="'.$this->id.'"';
+
+    $widget .= '>';
+
+    if (!empty($this->value))
+    {
+      $widget .= htmlspecialchars($this->value);
+    }
+
+    return $widget.'</textarea>';
+  }
+}
