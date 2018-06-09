d8bcce6950452fe490e5d4b4907ee2cf2c4783ab
antho <krobross_57@hotmail.fr>
2018-06-09 20:53:49 +0200

    création et ajout du fichier insertComment.php

---
+++ App/Frontend/Modules/News/Views/insertComment.php
@@ -0,0 +1,21 @@
+<div class="container">
+  <div class="row">
+    <div id="commentForm" class="col-lg-12 mainContent">
+      <h2 class="mainTitles">Ajouter un commentaire</h2>
+      <div class="container">
+        <div class="row">
+      <form class="col-lg-12" action="" method="post">
+
+
+          <?= $form ?>
+
+
+
+        <input type="submit" value="Commenter" />
+
+      </form>
+    </div>
+  </div>
+</div>
+</div>
+</div>
